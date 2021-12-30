54a88434d8fe2cc79adfc04feb96afd0528ddbf2
ammuanju97 <anjutennyson@gmail.com>
2021-12-30 17:28:58 +0530

    working on login

--- pension_user_authentication/views.py
+++ pension_user_authentication/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from django.core.mail import send_mail
-
-from .serializers import UserRegistrationSerializer, AccountActivationSerializer
-
+from .models import UserAccount
+from .serializers import ( UserRegistrationSerializer, AccountActivationSerializer,
+ UserLoginSerializer
+)
 
 # View for User Registration
 class PensionUserRegister(generics.GenericAPIView):
@@ -31,3 +32,40 @@ class PensionUserActivation(generics.GenericAPIView):
             "user" : serializer.data,
             "message" : "Your account activated successfully by OTP, Please Login!!",
             }, status = status.HTTP_201_CREATED)
+
+
+# View for User Login
+class PensionUserLogin(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    # def post(self, request, *args, **kwargs):
+    #     serializer = UserLoginSerializer(data = request.data)
+    #     print(request.data)
+       
+    #     serializer.is_valid()
+    #     print(serializer.data)
+    #     return Response({
+    #         "token" : 'token'
+    #     })
+
+    def post(self, request):
+        data = request.data
+        email_id = data.get('email_id',' ')
+        password = data.get('password',' ')
+
+        try:
+            user = UserAccount.objects.get(email_id = email_id)
+
+            if user:
+                # auth_token = jwt.encode({'email_id':user.email_id},'JWTSECRETKEYJWTSECRETKEYJWTSECRETKEY')
+
+                serializer = UserLoginSerializer(user)
+
+                data={
+                'user': serializer.data,
+                # 'token': auth_token
+                }
+
+                return Response(data,status=status.HTTP_200_OK)
+        except:
+            return Response({'details':'Invalid credentials'},status=status.HTTP_401_UNAUTHORIZED)
