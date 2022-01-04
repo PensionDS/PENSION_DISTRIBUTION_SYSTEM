e737f1924a598a0be74753e287c1f98f8c35dca3
vishnu-k-s <vishnusajeevks@gmail.com>
2022-01-04 18:38:49 +0530

    changepassword added

--- pension_user_authentication/serializers.py
+++ pension_user_authentication/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 import math, random
-from .sms import otp_by_sms, otp_by_email
+from .sms import otp_by_sms, otp_by_email, reset_password_by_email
 from django.contrib.auth.models import User
 from . models import UserAccountDetails
 
 
 # Serializer for User Registration
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    phone_number = serializers.IntegerField(write_only = True)
+    phone_number = serializers.CharField(write_only = True)
     confirm_password = serializers.CharField(write_only = True)
 
     class Meta:
@@ -26,8 +26,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         password = attrs.get('password', '')
         confirm_password = attrs.get('confirm_password', '')
         
-        # if len(str(phone_number)) != 13:
-        #     raise serializers.ValidationError({'phone_number' : ('phone_number  is not valid')})
+        if len(str(phone_number)) != 13:
+            raise serializers.ValidationError({'phone_number' : ('phone_number  is not valid')})
         
         if password != confirm_password:
             raise serializers.ValidationError({'password' : ('password mismatch, please enter same password')})
@@ -46,17 +46,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
         OTP = generateOTP()
 
-        # Calling function to send otp using  email
-        otp_by_email(validated_data['email'], OTP)
-
-        # Calling function to send otp using sms
-        # otp_by_sms(validated_data['phone_number'], OTP)
-
         user = User.objects.create(
            username = validated_data['username'],
            email =validated_data['email'],
             )
-            
+
         userreg = UserAccountDetails.objects.create(
             user = user,
             phone_number = validated_data['phone_number'],
@@ -65,10 +59,12 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
         user.set_password(validated_data['password'])
 
-        #otp_by_email(validated_data['email_id'], OTP)
+        # Calling function to send otp using  email
+        otp_by_email(validated_data['email'], OTP)
 
         # Calling function to send otp using sms
-        #otp_by_sms(validated_data['phone_number'], OTP)
+        otp_by_sms(validated_data['phone_number'], OTP)
+
 
         user.save()
         userreg.save()
@@ -101,4 +97,51 @@ class TokenGenerationSerializer(TokenObtainPairSerializer):
         token = super(TokenGenerationSerializer, cls).get_token(user)
 
         return token
+
+
+# Serializer for ForgetPassword
+class UserForgetPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length = 254)
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+
+        try:
+            user = User.objects.get(email = email)
+
+            if  user:
+                # Calling function to send change password link through email
+                reset_password_by_email(email) 
+
+        except:
+                raise serializers.ValidationError({'email_id' : ('This email is not registered')})
+
+        return super().validate(attrs)
+
+    def save(self):
+       pass
+
+
+# Serializer for Change Password
+class UserChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(max_length = 254)
+    new_password = serializers.CharField(max_length = 254)
+    confirm_password = serializers.CharField(max_length = 254)
+   
+    def validate(self, attrs):
+        old_password = attrs.get('old_password', '')
+        new_password = attrs.get('new_password', '')
+        confirm_password = attrs.get('confirm_password', '')
+
+        if new_password != confirm_password:
+            raise serializers.ValidationError({'password' : ('password mismatch, please enter same password')})
+        return super().validate(attrs)
+        
+    def create(self, validated_data): 
+        #user = User.objects.get(password=validated_data['old_password'])
+        user = User.objects.get(username=validated_data['old_password'])
+
+        user.set_password(validated_data['new_password'])
+        user.save()
+        return user
 
